e21bbf02b7d6992bd153cacce17f1a7439dbc1d6
Karel Rosseel <karel.rosseel@gmail.com>
2021-09-29 21:52:16 +0200

    vlaams

---
+++ WholeRepresentationMatch/js/Localization.js
@@ -0,0 +1,40 @@
+//This is where we will store localization for now
+localization =
+	{
+		openingDialogTitle:["Kies je nummer-symbool die je wilt bij elkaar matchen.","Choisir les nombres que vous voulez faire correspondre."],
+		openingDialogSecondInstruction:["Choose two or more representations.","Choisir au moins deux reprÃ©sentations."],
+		gameName:["Whole Number Representation Match", "ReprÃ©sentations des nombres naturels\nJeu d'association"],
+		play:["Play","Jouer"],
+		faceUp:["Face\nup","Face\nvisible"],
+		faceDown:["Face\ndown","Face\ncachÃ©e"],
+		level:["Numbers:", "Nombres :"],
+		levelDescriptions:[["0 to 5 ", "0 to 10 ", "0 to 20 ", "20 to 50 "],["0 Ã  5 ", "0 Ã  10 ", "0 Ã  20 ", "20 Ã  50 "]]
+	};
+
+localization.gameContainer = {};
+localization.gameContainer.pickACard = ["Choose a card.", "Choisir une carte."];
+localization.gameContainer.chooseAMatchingCard = ["Find a match.", "Trouver une correspondance."];
+localization.gameContainer.madeAMatch = ["It's a match!", "Ã‡a correspond!"];
+localization.gameContainer.madeAMatchLater = ["It's a match!  Continue.", "Ã‡a correspond! Allons-y!"];
+localization.gameContainer.notAMatch = ["Not a match. Try again.",
+"Ne correspond pas. Essayer Ã  nouveau."];
+localization.gameContainer.matchedAll = ["You matched all the cards!", "Toutes les cartes ont correspondu!"];
+
+localization.informationDialog = {};
+localization.informationDialog.informationTitle = ["v1.0.0", "v1.0.0"];
+localization.informationDialog.infoDialogTitle = ["Whole Number Representation Match", "ReprÃ©sentations des nombres naturels"];
+localization.informationDialog.supportButton = ["Support Page", "Page de support"];
+localization.informationDialog.feedbackButton = ["Feedback Form", "Formulaire de rÃ©troaction"];
+localization.informationDialog.mathiesToolsButton = ["mathies Games", "Jeux de mathies"];
+localization.informationDialog.infoDialogIntro = ["For additional help visit the", "Pour de l'aide supplÃ©mentaire veuillez visiter"];
+localization.informationDialog.infoDialogMainMessage = ["This mathies learning tool is being developed for the App Store, Google Play Store and as a web page for desktop computers.", "Cet outil d'apprentissage de mathies est dÃ©veloppÃ© pour les magasins 'App Store' et 'Google Play Store' ainsi que pour une page web pour des ordinateurs personnels."];
+localization.informationDialog.infoDialogMoreAvailable = ["More games are available at mathies.ca", "D'autres jeux sont disponibles Ã  mathies.ca."];
+localization.informationDialog.copyright = ["\u00A9 The Ontario Association for Mathematics Education, "+(new Date()).getFullYear()+",\nin partnership with: \n   - the Ontario Ministry of Education and\n   - the Council of Ontario Directors of Education", "\u00A9 Association Ontarienne pour L'Enseignement des Mathematiques,\n "+(new Date()).getFullYear()+", en partenariat avec :\n   - le MinistÃ¨re de lâ€™Ã‰ducation de lâ€™Ontario et\n   - CODE"];
+
+localization.gameOverDialog = {};
+localization.gameOverDialog.playAgain = ["Play:", "Jouer :"]
+localization.gameOverDialog.sameCards = ["Same Cards", "MÃªmes cartes"];
+localization.gameOverDialog.sameLevel = ["Same Choices", "MÃªmes choix"];
+localization.gameOverDialog.newLevel = ["New Choices", "Nouveaux choix"];
+localization.gameOverDialog.mathies = ["mathies.ca Games", "Jeux Ã  mathies.ca"];
+localization.gameOverDialog.gameOver = ["Game Over", "Jeu terminÃ©"];
